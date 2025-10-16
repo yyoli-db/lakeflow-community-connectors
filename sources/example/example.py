b764a6774b8f03c2309e2875e67c5ee469639918
Yong Li <yong.li@databricks.com>
2025-10-16 16:00:13 -0700

    u

--- sources/example/example.py
+++ sources/example/example.py
@@ -5,6 +5,7 @@ from pyspark.sql.types import StructType, StructField, IntegerType, StringType
 
 
 # This is an example implementation of the LakeflowConnect class.
+# Test a change
 class LakeflowConnect():
     def __init__(self, options: Dict[str, str]) -> None:
         """
