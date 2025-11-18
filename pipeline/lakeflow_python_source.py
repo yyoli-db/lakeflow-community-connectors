44de872827166b6904225c664cfcd3487b29fc72
Yong Li <yong.li@databricks.com>
2025-11-17 21:53:14 -0800

    u

--- pipeline/lakeflow_python_source.py
+++ pipeline/lakeflow_python_source.py
@@ -36,7 +36,7 @@ class LakeflowStreamReader(SimpleDataSourceStreamReader):
 
     def read(self, start: dict) -> (Iterator[tuple], dict):
         records, offset = self.lakeflow_connect.read_table(
-            self.options["tableName"], start
+            self.options["tableName"], start, self.options
         )
         rows = map(lambda x: parse_value(x, self.schema), records)
         return rows, offset
@@ -67,7 +67,9 @@ class LakeflowBatchReader(DataSourceReader):
         if self.table_name == METADATA_TABLE:
             all_records = self._read_table_metadata()
         else:
-            all_records, _ = self.lakeflow_connect.read_table(self.table_name, None)
+            all_records, _ = self.lakeflow_connect.read_table(
+                self.table_name, None, self.options
+            )
 
         rows = map(lambda x: parse_value(x, self.schema), all_records)
         return iter(rows)
@@ -77,7 +79,7 @@ class LakeflowBatchReader(DataSourceReader):
         table_names = [o.strip() for o in table_name_list.split(",") if o.strip()]
         all_records = []
         for table in table_names:
-            metadata = self.lakeflow_connect.read_table_metadata(table)
+            metadata = self.lakeflow_connect.read_table_metadata(table, self.options)
             all_records.append({"tableName": table, **metadata})
         return all_records
 
@@ -104,7 +106,7 @@ class LakeflowSource(DataSource):
             )
         else:
             # Assuming the LakeflowConnect interface uses get_table_schema, not get_table_details
-            return self.lakeflow_connect.get_table_schema(table)
+            return self.lakeflow_connect.get_table_schema(table, self.options)
 
     def reader(self, schema: StructType):
         return LakeflowBatchReader(self.options, schema, self.lakeflow_connect)
